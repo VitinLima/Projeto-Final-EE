5babbef753ce5535d7a17d20c2e7afa05ac1b499
Vítor Lima <aguirra.vitor@gmail.com>
2022-10-02 23:12:15 -0300

    Testing version

--- main.c
+++ main.c
@@ -148,6 +148,8 @@ void TMR0_Interrupt(){
             EUSART_Write(data_tx[i]);
         }
     }
+    txMAX7219(0x0F,0x01); // Display-Test = 1
+    txMAX7219(0x0F,0x00); // Display-Test = 0
 }
 
 void TMR4_Interrupt(){ // Espera de 500 ms para mudança de direção
@@ -259,11 +261,11 @@ void main(void)
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
     
-    initMAX7219();
     DIR_SetHigh();
     PWM3_LoadDutyValue(409);
     __delay_ms(2000);
     updateMotor();
+//    initMAX7219();
     
     uint8_t receivedData = 0;
     
